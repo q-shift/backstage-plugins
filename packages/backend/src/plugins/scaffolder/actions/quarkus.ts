e96a06cc92496f38fffeb49f25d3bc89de67daea
Ioannis Canellos <iocanel@gmail.com>
2023-11-10 16:00:28 +0200

    fix: the quarkus action that clones quickstarts

--- packages/backend/src/plugins/scaffolder/actions/quarkus.ts
+++ packages/backend/src/plugins/scaffolder/actions/quarkus.ts
@@ -15,26 +15,44 @@ export const cloneQuarkusQuickstart = () => {
     id: 'quarkus:quickstart:clone',
     schema: {
       input: z.object({
-        quickstartName: z
-        .string()
-        .describe('The name of the quickstart'),
+        values: z.object({
+          groupId: z.string().optional().describe('The groupId'),
+          artifactId: z.string().optional().describe('The artifactId'),
+          version: z.string().optional().describe('The version'),
+          quickstartName: z.string().describe('The name of the quickstart'),
+        }),
       }),
     },
 
     async handler(ctx) {
-      await clone({
-        fs,
-        http,
-        dir: `./kafka-quickstart`,
-        url: `https://github.com/quarkusio/quarkus-quickstarts.git`,
-        noTags: true,
-        singleBranch: true, // Optional: Only clone a single branch (default is false)
+      ctx.createTemporaryDirectory().then((tempDir) => {
+        const cloneDir = path.join(tempDir, 'downloaded.zip');
+        clone({
+          fs,
+          http,
+          dir: cloneDir,
+          url: `https://github.com/quarkusio/quarkus-quickstarts.git`,
+          noTags: true,
+          singleBranch: true, // Optional: Only clone a single branch (default is false)
+        }).then(() => {
+            //Copy all files from cloneDir to outputDir
+            const quickstartDir = path.join(cloneDir, ctx.input.values.quickstartName);
+            fs.copySync(quickstartDir, ctx.workspacePath);
+            //replace the artifactId in the pom.xml
+            const pomPath = path.join(outputDir, 'pom.xml');
+            const xml = fs.readFileSync(pomPath, 'utf8');
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(xml, 'text/xml');
+            doc.getElementsByTagName('groupId')[0].textContent = ctx.input.values.groupId;
+            doc.getElementsByTagName('artifactId')[0].textContent = ctx.input.values.artifactId;
+            doc.getElementsByTagName('version')[0].textContent = ctx.input.values.version;
+            //write doc as XML back to file
+            fs.writeFileSync(pomPath, doc.documentElement.outerHTML);
+        });
       });
     },
   });
 };
-
-
 
 export const createQuarkusApp = () => {
   return createTemplateAction({
@@ -45,18 +63,26 @@ export const createQuarkusApp = () => {
           groupId: z.string().optional().describe('The groupId'),
           artifactId: z.string().optional().describe('The artifactId'),
           version: z.string().optional().describe('The version'),
+          buildTool: z.enum(['MAVEN', 'GRADLE', 'GRADLE_KOTLIN_DSL']).optional().describe('The buildTool to use'),
+          extensions: z.array(z.string()).optional().describe('The extensions'),
+          javaVersion: z.string().optional().describe('The java version'),
+          targetPath: z.string().optional().describe('The target path'),
         }),
-        targetPath: z.string().optional().describe('The target path'),
       }),
     },
 
     async handler(ctx) {
-
       const apiUrl = 'https://code.quarkus.io/api/download'; // Replace with your API endpoint
       const postData = {
-        // Your POST data here
+        groupId: ctx.input.values.groupId ? ctx.input.values.groupId : 'org.amce',
+        artifactId: ctx.input.values.artifactId ? ctx.input.values.artifactId : 'code-with-quarkus',
+        version: ctx.input.values.version ? ctx.input.values.version : '1.0.0-SNAPSHOT',
+        buildTool: ctx.input.values.buildTool ? ctx.input.values.buildTool : 'MAVEN',
+        javaVersion: ctx.input.values.javaVersion ? ctx.input.values.javaVersion : '11',
+        extensions: ctx.input.values.extensions ? ctx.input.values.extensions : []
       };
 
+      var appDirName = ctx.input.values.artifactId ? ctx.input.values.artifactId : 'code-with-quarkus';
       const headers = {
         'Content-Type': 'application/json', // Adjust as needed
       };
@@ -82,8 +108,8 @@ export const createQuarkusApp = () => {
                     if (zipFile) {
                       zipFile.async('nodebuffer').then(function(content) {
                         //if filename starts with code-with-quarkus directory remove it
-                        if (filename.startsWith('code-with-quarkus')) {
-                          filename = filename.replace('code-with-quarkus/', '');
+                        if (filename.startsWith(appDirName)) {
+                          filename = filename.replace(appDirName + '/', '');
                         }
                         var dest = path.join(outputDir, filename);
                         //Create directories if needed
