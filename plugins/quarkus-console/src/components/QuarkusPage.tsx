e40a9ecea6e42711a2b47f28d5b076cfae76abaf
Ioannis Canellos <iocanel@gmail.com>
2024-03-20 17:12:40 +0200

    chore: rename QuarkusComponent to QuarkusPage

---
+++ plugins/quarkus-console/src/components/QuarkusPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { QuarkusApplicationInfo } from './QuarkusApplicationInfo';
+
+export const QuarkusPage = () => {
+    return (
+      <QuarkusApplicationInfo/>
+    );
+};
+
